4ffb34ef047bd8dfe51b2a90cd51fc8a028f1c0d
Ishita Trivedi <146318817+Ishitatrivedi23@users.noreply.github.com>
2025-03-20 16:29:34 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: "13.53.199.34", //public ip
+  host: "16.16.146.76", //public ip
   user: "root",
   password: "Aditi@1122",
   port: 3306,   
